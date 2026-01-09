55804b675d31e370d11c303931301547b4ad036b
lamtung16 <nguyenlamtung10@gmail.com>
2026-01-09 09:43:52 -0700

    add pelt

--- opart_lda.py
+++ opart_lda.py
@@ -45,6 +45,37 @@ def opart(sequence, lda):
     return set_of_chpnt[1:-1] - 1
 
 
+# # opart
+# def opart(sequence, lda):
+#     sequence = np.append(0, sequence)
+#     y, z = get_cumsum(sequence)              # cumsum vector
+#     sequence_length = len(sequence) - 1      # length of sequence
+
+#     # Set up
+#     C = np.zeros(sequence_length + 1)
+#     C[0] = -lda
+
+#     tau_star = np.zeros(sequence_length + 1, dtype=int)
+
+#     # Main loop
+#     for t in range(1, sequence_length + 1):
+#         best_value = np.inf
+#         best_tau = 0
+
+#         # evaluate all candidates
+#         for tau in range(t):
+#             value = C[tau] + lda + L(tau + 1, t, y, z)
+#             if value < best_value:
+#                 best_value = value
+#                 best_tau = tau
+
+#         C[t] = best_value
+#         tau_star[t] = best_tau
+
+#     set_of_chpnt = trace_back(tau_star[1:])
+#     return set_of_chpnt[1:-1] - 1
+
+
 def get_T(t, neg_start, neg_end, pos_start, pos_end): 
 
     # if t is just outside of pos region
@@ -88,5 +119,78 @@ def lopart(sequence, neg_start, neg_end, pos_start, pos_end, lda):
         tau_star[t] = po_chpnt[np.argmin(V)]                                    # update tau_star
 
     # get set of changepoints
+    set_of_chpnt = trace_back(tau_star[1:])
+    return set_of_chpnt[1:-1] - 1
+
+
+# def pelt(sequence, lda):
+#     sequence = np.append(0, sequence)
+#     y, z = get_cumsum(sequence)
+#     n = len(sequence) - 1
+
+#     # Cost array
+#     C = np.zeros(n + 1)
+#     C[0] = -lda
+
+#     # Backpointer
+#     tau_star = np.zeros(n + 1, dtype=int)
+
+#     # Candidate changepoints
+#     R = [0]
+
+#     for t in range(1, n + 1):
+#         best_value = np.inf
+#         best_tau = 0
+
+#         # Evaluate only candidates in R
+#         for tau in R:
+#             value = C[tau] + lda + L(tau + 1, t, y, z)
+#             if value < best_value:
+#                 best_value = value
+#                 best_tau = tau
+
+#         C[t] = best_value
+#         tau_star[t] = best_tau
+
+#         # Pruning step
+#         new_R = []
+#         for tau in R:
+#             if C[tau] + L(tau + 1, t, y, z) <= C[t]:
+#                 new_R.append(tau)
+
+#         new_R.append(t)
+#         R = new_R
+
+#     set_of_chpnt = trace_back(tau_star[1:])
+#     return set_of_chpnt[1:-1] - 1
+
+def pelt(sequence, lda):
+    sequence = np.append(0, sequence)
+    y, z = get_cumsum(sequence)
+    n = len(sequence) - 1
+
+    C = np.zeros(n + 1)
+    C[0] = -lda
+
+    tau_star = np.zeros(n + 1, dtype=int)
+
+    # Candidate set
+    R = np.array([0], dtype=int)
+
+    for t in range(1, n + 1):
+
+        # ---- Vectorized evaluation over R ----
+        V = C[R] + lda + L(R + 1, t, y, z)
+
+        idx = np.argmin(V)
+        C[t] = V[idx]
+        tau_star[t] = R[idx]
+
+        # ---- Vectorized pruning ----
+        R = R[V - lda <= C[t]]
+
+        # Add current time point
+        R = np.append(R, t)
+
     set_of_chpnt = trace_back(tau_star[1:])
     return set_of_chpnt[1:-1] - 1
